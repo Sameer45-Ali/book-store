baf5eb0b4c6df248beaa4a6cc5e507ddae901f9a
Sameer45-Ali <sameer2659110@gmail.com>
2025-07-02 14:28:13 +0500

    added some changes to button

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -3,12 +3,13 @@ import { BsArrowLeft } from 'react-icons/bs';
 
 const BackButton = ({ destination = '/' }) => {
   return (
-    <div className='flex'>
+    <div className='mb-4'>
       <Link
         to={destination}
-        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
+        className='inline-flex items-center gap-2 bg-sky-700 hover:bg-sky-600 transition-colors text-white px-4 py-2 rounded-md shadow-md'
       >
-        <BsArrowLeft className='text-2xl' />
+        <BsArrowLeft className='text-xl' />
+        <span>Back</span>
       </Link>
     </div>
   );
